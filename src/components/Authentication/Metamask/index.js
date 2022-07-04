ee83826a894758c1806a5c5f0dab879606b3398b
iamdev-james <jamesoluwabukola87@gmail.com>
2022-07-04 15:10:39 -0100

    Added functionalties to the metamask authentication

--- src/components/Authentication/Metamask/index.js
+++ src/components/Authentication/Metamask/index.js
@@ -1,8 +1,40 @@
-import { useEffect, useState } from "react";
-import styles from "./metamask-auth.module.css";
+import React, { useEffect, useState } from "react";
+import { FaHeading } from 'react-icons/fa';
 
+function isMobileDevice() {
+  return 'ontouchstart' in window || 'onmsgesturechange' in window;
+}
 
-function checkIfWalletIsConnected(onConnected) {}
+async function connect(onConnected) {
+  if (!window.ethereum) {
+    alert("Get MetaMask!");
+    return;
+  }
+
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+
+  onConnected(accounts[0]);
+}
+
+async function checkIfWalletIsConnected(onConnected) {
+  if (window.ethereum) {
+    const accounts = await window.ethereum.request({
+      method: "eth_accounts",
+    });
+
+    if (accounts.length > 0) {
+      const account = accounts[0];
+      onConnected(account);
+      return;
+    }
+
+    if (isMobileDevice()) {
+      await connect(onConnected);
+    }
+  }
+}
 
 
 export default function MetaMaskAuth({ onAddressChanged }) {
@@ -12,15 +44,45 @@ export default function MetaMaskAuth({ onAddressChanged }) {
     checkIfWalletIsConnected(setUserAddress);
   }, []);
 
-  useEffect(() => {
-    onAddressChanged(userAddress);
-  }, [userAddress]);
-
   return userAddress ? (
     <div>
       Connected with <Address userAddress={userAddress} />
     </div>
   ) : (
      <Connect setUserAddress={setUserAddress}/>
+  );
+}
+
+
+function Connect({ setUserAddress }) {
+  if (isMobileDevice()) {
+    const dappUrl = "metamask-auth.ilamanov.repl.co"; // TODO enter your dapp URL. For example: https://uniswap.exchange. (don't enter the "https://")
+    const metamaskAppDeepLink = "https://metamask.app.link/dapp/" + dappUrl;
+    return (
+      <a href={metamaskAppDeepLink}>
+         <button className='px-7 py-3 rounded-xl bg-secondary flex flex-row justify-center items-center text-black text-sm md:text-lg font-bold md:font-medium hover:bg-gray-300'>
+          <FaHeading className='mr-4 text-2xl font-bold text-red-500' />
+            Sign in with MetaMask
+        </button>
+      </a>
+    );
+  }
+
+  
+  return (
+    <button
+    onClick={() => connect(setUserAddress)}
+    className='px-7 py-3 rounded-xl bg-secondary flex flex-row justify-center items-center text-black text-sm md:text-lg font-bold md:font-medium hover:bg-gray-300'>
+      <FaHeading className='mr-4 text-2xl font-bold text-red-500' />
+        Sign in with MetaMask
+    </button>
+  );
+}
+
+
+function Address({ userAddress }) {
+  return (
+    // Give the account showing a style or perform some logic here
+    <span className="">{userAddress.substring(0, 5)}…{userAddress.substring(userAddress.length - 4)}</span>
   );
 }
